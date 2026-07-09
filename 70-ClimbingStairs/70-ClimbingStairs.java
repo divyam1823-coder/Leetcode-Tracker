ba216967e94fa3ea9776d67b87e2aea3298ab3a1
divya <divyam1823@gmail.com>
2026-07-09 15:19:37 +0530

    Create 70-ClimbingStairs

---
+++ 70-ClimbingStairs/70-ClimbingStairs.java
@@ -0,0 +1,18 @@
+// Last updated: 7/9/2026, 3:19:36 PM
+class Solution {
+    public int climbStairs(int n) {
+        if (n <= 3) return n;
+
+        int prev1 = 3;
+        int prev2 = 2;
+        int cur = 0;
+
+        for (int i = 3; i < n; i++) {
+            cur = prev1 + prev2;
+            prev2 = prev1;
+            prev1 = cur;
+        }
+
+        return cur;        
+    }
+}

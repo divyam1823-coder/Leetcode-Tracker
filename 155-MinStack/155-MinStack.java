207ba4d091fbde470435035dd125d17d8f66e02c
divya <divyam1823@gmail.com>
2026-07-09 15:18:36 +0530

    Create 155-MinStack

---
+++ 155-MinStack/155-MinStack.java
@@ -0,0 +1,19 @@
+// Last updated: 7/9/2026, 3:18:35 PM
+class MinStack {
+    Stack<Long> s = new Stack<>();
+    long minEle;
+    public void push(int val) {
+        if (s.isEmpty()) { s.push((long)val); minEle = val; }
+        else if (val < minEle) { s.push(2L*val - minEle); minEle = val; }
+        else s.push((long)val);
+    }
+    public void pop() {
+        long t = s.pop();
+        if (t < minEle) minEle = 2*minEle - t;
+    }
+    public int top() {
+        long t = s.peek();
+        return t < minEle ? (int)minEle : (int)t;
+    }
+    public int getMin() { return (int)minEle; }
+}

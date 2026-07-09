fca8a9418e50b39a5edd5031ed497d6c5cd38b28
divya <divyam1823@gmail.com>
2026-07-09 15:15:41 +0530

    Create 2238-ANumberAfterADoubleReversal

---
+++ 2238-ANumberAfterADoubleReversal/2238-ANumberAfterADoubleReversal.cpp
@@ -0,0 +1,17 @@
+// Last updated: 7/9/2026, 3:15:40 PM
+class Solution {
+public:
+    bool isSameAfterReversals(int num) {
+        
+
+        
+        if (num==0){
+            return true;
+        }else if(num%10==0){
+            return false;
+        }
+        else{
+            return true;
+        }
+    }
+};

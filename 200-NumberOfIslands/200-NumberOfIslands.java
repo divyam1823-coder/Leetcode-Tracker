e88787ab63d2a89e97d6d203f1adfca6c9b158d0
divya <divyam1823@gmail.com>
2026-07-09 15:17:54 +0530

    Create 200-NumberOfIslands

---
+++ 200-NumberOfIslands/200-NumberOfIslands.java
@@ -0,0 +1,42 @@
+// Last updated: 7/9/2026, 3:17:53 PM
+class Solution {
+    public int numIslands(char[][] grid) {
+       
+        int m=grid.length;
+        int n=grid[0].length;
+        boolean [][] visited=new boolean[m][n];
+         int ans=0;
+        for(int i=0;i<m;i++){
+            for (int j=0;j<n;j++){
+                if(grid[i][j]=='1' && visited[i][j]==false){
+                    ans++;
+                    bfs(i,j,visited,grid,m,n);
+                }
+            }
+            
+        }
+        return ans;
+    } 
+    public void bfs(int i,int j,boolean [][]visited,char[][]grid,int m,int n){
+        Queue<int[]> q=new LinkedList<>();
+        q.add(new int []{i,j});
+        visited[i][j]=true;
+        int [][] dir={{-1,0},{1,0},{0,-1},{0,1}};
+        while(q.size()>0){
+            int curr[]=q.poll();
+            int x=curr[0];
+            int y=curr[1];
+            for(int k=0;k<4;k++){
+                int nextrow=x+dir[k][0];
+                int nextcol=y+dir[k][1];
+                if(nextrow<m && nextrow>=0 && nextcol<n && nextcol>=0 && visited[nextrow][nextcol]==false && grid[nextrow][nextcol]=='1'){
+                    q.add(new int[]{nextrow,nextcol});
+                    visited[nextrow][nextcol]=true;
+
+                }
+            }
+        }
+        
+
+    }
+}

4daca6169119badb4ffd54777579ec8a85c8a6a1
divya <divyam1823@gmail.com>
2026-07-09 15:18:17 +0530

    Create 180-ConsecutiveNumbers

---
+++ 180-ConsecutiveNumbers/180-ConsecutiveNumbers.sql
@@ -0,0 +1,12 @@
+-- Last updated: 7/9/2026, 3:18:16 PM
+# Write your MySQL query statement below
+# Write your MySQL query statement below
+with cte as (
+    select num,
+    lead(num,1) over() num1,
+    lead(num,2) over() num2
+    from logs
+
+)
+
+select distinct num ConsecutiveNums from cte where (num=num1) and (num=num2)
